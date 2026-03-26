7a78f28854e03385438e388527d55d50678a05dd
Ubuntu <ubuntu@ip-10-0-0-39.ec2.internal>
2026-03-26 22:54:16 +0000

    final update

--- Admin/src/pages/Login/Login.jsx
+++ Admin/src/pages/Login/Login.jsx
@@ -36,32 +36,30 @@ const Login = () => {
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleLogin}>
-        <h2>Admin Login</h2>
-
+        <h2>Login</h2>
         <div className="form-group">
-          <label>Email</label>
+          <label htmlFor="email">Email</label>
           <input
             type="email"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
-
         <div className="form-group">
-          <label>Password</label>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
+            id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
         </div>
-
-        <button type="submit">Login</button>
+        <button type="submit" className="login-button">Login</button>
       </form>
     </div>
   );
 };
-
 export default Login;
